538862c3bffb3cc99095b744bdea9ed562c8e2ff
Will Maguire <will_maguire99@protonmail.com>
2025-10-07 21:41:50 +0100

    Refactor envman load scripts and update Zsh configuration remove Powerlevel10k

--- .config/envman/load.fish
+++ .config/envman/load.fish
@@ -1,16 +1,16 @@
-# Generated for envman. Do not edit.
+# # Generated for envman. Do not edit.
 
-touch -a ~/.config/envman/PATH.env
-touch -a ~/.config/envman/ENV.env
-touch -a ~/.config/envman/alias.env
-touch -a ~/.config/envman/function.fish
+# touch -a ~/.config/envman/PATH.env
+# touch -a ~/.config/envman/ENV.env
+# touch -a ~/.config/envman/alias.env
+# touch -a ~/.config/envman/function.fish
 
-not set -q ENVMAN_LOAD; and source ~/.config/envman/ENV.env
-not set -q ENVMAN_LOAD; and source ~/.config/envman/PATH.env
+# not set -q ENVMAN_LOAD; and source ~/.config/envman/ENV.env
+# not set -q ENVMAN_LOAD; and source ~/.config/envman/PATH.env
 
-set -x ENVMAN_LOAD 'loaded'
+# set -x ENVMAN_LOAD 'loaded'
 
-not set -q g_envman_load_fish; and source ~/.config/envman/function.fish
-not set -q g_envman_load_fish; and source ~/.config/envman/alias.env
+# not set -q g_envman_load_fish; and source ~/.config/envman/function.fish
+# not set -q g_envman_load_fish; and source ~/.config/envman/alias.env
 
-set -g g_envman_load_fish 'loaded'
+# set -g g_envman_load_fish 'loaded'
--- .zshrc
+++ .zshrc
@@ -1,4 +1,1 @@
-source $HOME/.config/zsh/cachyos-config.zsh
-
-# Generated for envman. Do not edit.
-[ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
+source $HOME/.config/zsh/wm.zsh
